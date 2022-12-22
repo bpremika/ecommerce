2bbfc904b845a69e52840faea23b062033ebdd86
Premika Yutinatorn <premika.yu@gmail.com>
2022-12-22 12:32:39 +0700

    feats: first commit

---
+++ backend/index.ts
@@ -0,0 +1,12 @@
+import express, { Express, NextFunction, Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+});
